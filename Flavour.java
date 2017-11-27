ab49f5d3dabcb40df4fd3072f3e965614f48d779
ryanmansoor <34032322+ryanmansoor@users.noreply.github.com>
2017-11-27 14:49:26 +0000

    Create Flavour.java

---
+++ Flavour.java
@@ -0,0 +1,13 @@
+package iScream;
+
+public class Flavour {
+	public int FlavourId;
+	public String FlavourName;
+	public double ScoopPrice;
+	public String FlavourType;
+	public int WeeklyStock;
+	public int CurrentStock;
+	
+	
+
+}
